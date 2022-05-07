af6ae80b141015fe6be530ba0d4a1835f076b780
David14334 <98358777+David14334@users.noreply.github.com>
2022-05-06 21:16:44 -0500

    Update urls.py

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -7,7 +7,7 @@ app_name = 'pizzas'
 
 urlpatterns = [    
     path('',views.index, name='index'),
-    path('pizzas',views.MainApp, name='pizzas'),
+    path('pizzas',views.pizzas, name='pizzas'),
     path('pizzas/<int:pizza_id>/',views.pizza,name='pizza'),
     path('comment/<int:pizza_id>/',views.comment, name='comment'),
     ]
